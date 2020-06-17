f4bb2f46c594785c3e9a5b73e5b13c85d38d5710
Luiz Santos <luizcsbh@gmail.com>
2020-06-16 23:07:11 -0300

    Update routes.jsx

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, IndexRouter, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
 import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
@@ -8,7 +8,7 @@ import Rendimento from '../rendimento/rendimento'
 export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={AuthOrApp}>
-            <IndexRouter path='/' component={Dashboard} />
+            <IndexRoute  component={Dashboard} />
             <Route path='rendimentos' component={Rendimento}/>
         </Route>
         <Redirect from='*' to='/' />
